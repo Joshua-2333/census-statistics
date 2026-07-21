32aca5754496a20f8d84d28e97ccb3490f9e642d
Joshua-2333 <jaflores.2333@gmail.com>
2026-07-21 15:00:19 -0500

    Add print statements to display first data row details

--- readCensusExcel.py
+++ readCensusExcel.py
@@ -15,3 +15,8 @@ print(f"Worksheet: {sheet.title}")
 print(f"Rows: {sheet.max_row}")
 print(f"Columns: {sheet.max_column}")
 
+print("\nFirst data row:")
+
+print("State :", sheet["B2"].value)
+print("County:", sheet["C2"].value)
+print("Pop   :", sheet["D2"].value)
